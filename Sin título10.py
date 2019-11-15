310fc0445ae63912c547f0b261c9dfedf90e08b8
Mateo Brito <56989615+Mateob20@users.noreply.github.com>
2019-11-14 22:29:29 -0500

    Add files via upload

---
+++ Sin título10.py
@@ -0,0 +1,13 @@
+while True:
+    x = input("Enter a number to count to:")
+    x=int(x)
+    if x == 'q' or x == 'quit':
+        break
+    
+y=1
+while True:
+    print (y)
+    y=y+1
+    if y>x:
+        break
+        
